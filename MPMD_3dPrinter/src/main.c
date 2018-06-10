e0ddc9271540c3cd145ac2731c176d92cbeef59a
kulivontot <kulivontot@hotmail.com>
2018-06-09 20:23:30 -0400

    Added refresh WDG timer to systick handler Added vector remapping to the startup code to handle being loaded from bootloader, hardcoded for now Added changes in .ld file to account for being started from the bootloader

--- MPMD_3dPrinter/src/main.c
+++ MPMD_3dPrinter/src/main.c
@@ -10,9 +10,34 @@
 //Includes
 #include "main.h"
 #include "Marlin_export.h"
+//Private defines
+#define APPLICATION_ADDRESS     (uint32_t)0x08002000
+//#define APPLICATION_ADDRESS		  (uint32_t)0x08000000
+//Private variables
+__IO uint32_t VectorTable[48] __attribute__((section(".RAMVectorTable")));
+
+static inline void remapVectorTable(void)
+{
+	for(uint8_t i = 0; i < 48; i++)
+	{
+	    VectorTable[i] = *(__IO uint32_t*)(APPLICATION_ADDRESS + (i<<2));
+	}
+	  /* Enable the SYSCFG peripheral clock*/
+	__HAL_RCC_APB2_FORCE_RESET();
+	  /* Remap SRAM at 0x00000000 */
+	__HAL_SYSCFG_REMAPMEMORY_SRAM();
+	RCC->AHBENR = 1<<2 |1<<4 | 1<<18;
+	 if((WWDG->CR == 0xC0) && (WWDG->CR & 0x80))
+		 WWDG->CR = 0x000000ff;
+//			 HAL_WWDG_Refresh(&WwdgHandle);
+	 if(IWDG->RLR)
+		 IWDG->RLR = 0x0000AAAAU;
+	 __HAL_RCC_APB2_RELEASE_RESET();
+}
 
 int main(void)
 {
+	remapVectorTable();
 	setup();
 	for(;;)
 	{
